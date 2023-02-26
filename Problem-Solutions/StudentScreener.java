d57d6a40986f63d25320b1ad6c8db619ec47269f
Sachin Kanzariya <114856060+sachin027@users.noreply.github.com>
2023-02-26 17:15:35 +0530

    Update StudentScreener.java

--- Problem-Solutions/StudentScreener.java
+++ Problem-Solutions/StudentScreener.java
@@ -14,52 +14,76 @@ public class StudentScreener {
   
     public static String getEligibleStudents(int totalStudents, String[] students, int[] marks, String[] examResults, int totalMarks, float requiredPercentile) throws Exception {
       //TODO: Implement this function and remove below line, which throws exception
-    String ans = "";
-    int k = 0;
-        int max = 0;
-        for(int i=0;i<6;i++){
-            max=marks[i];
-            k=i;
-            for(int j=i;j<7;j++){
-                if(marks[j]>=max){
-                    k = j;
-                    max=marks[j];
-                }
-            }
-            int temp = marks[i];
-            marks[i]=max;
-            marks[k]=temp;
-
-            String temp1 = students[i];
-            students[i]=students[k];
-            students[k]=temp1;
-
-            String pass = examResults[i];
-            examResults[i]=examResults[k];
-            examResults[k]=pass;
         
-        }
+        //for specific problem
+        
+//     String ans = "";
+//     int k = 0;
+//         int max = 0;
+//         for(int i=0;i<6;i++){
+//             max=marks[i];
+//             k=i;
+//             for(int j=i;j<7;j++){
+//                 if(marks[j]>=max){
+//                     k = j;
+//                     max=marks[j];
+//                 }
+//             }
+//             int temp = marks[i];
+//             marks[i]=max;
+//             marks[k]=temp;
+
+//             String temp1 = students[i];
+//             students[i]=students[k];
+//             students[k]=temp1;
+
+//             String pass = examResults[i];
+//             examResults[i]=examResults[k];
+//             examResults[k]=pass;
+        
+//         }
     
-    double y = 0.0;
-    int[] arr = new int[7];
-    for(int i = 0;i<7;i++){
-            y = (double)(7-1-i)/7*100;
+//     double y = 0.0;
+//     int[] arr = new int[7];
+//     for(int i = 0;i<7;i++){
+//             y = (double)(7-1-i)/7*100;
             
-            if(y>=50.0 && examResults[i].equals("Passed")){
-                arr[i]=1;
-            }
-            else{
-                arr[i]=0;
-            }
+//             if(y>=50.0 && examResults[i].equals("Passed")){
+//                 arr[i]=1;
+//             }
+//             else{
+//                 arr[i]=0;
+//             }
+//     }
+
+//     for(int i = 0;i<7;i++){
+//         if(arr[i]==1 && ans.equals("")){
+//             ans =  "\"" + students[i];
+//         }else if(arr[i]==1){
+//             ans = ans + ","+ students[i];
+//         }
+//     }
+//     return ans+"\"";
+        
+        //updated solution !!!!!!!!!
+        
+        
+    int n = students.length;
+    Map<Integer, String> studentList = new HashMap<>();
+    Map<Integer, String> studentResult = new HashMap<>();
+    for (int i = 0; i < students.length; i++) {
+        studentList.put(marks[i], students[i]);
+        studentResult.put(marks[i], examResults[i]);
     }
 
-    for(int i = 0;i<7;i++){
-        if(arr[i]==1 && ans.equals("")){
-            ans =  "\"" + students[i];
-        }else if(arr[i]==1){
-            ans = ans + ","+ students[i];
-        }
+    int rank = n - (int) (requiredPercentile * n / 100);
+    Arrays.sort(marks);
+
+    List<String> res = new ArrayList<>();
+    for (int i = n - 1; i > n - rank; i--) {
+        if (studentResult.get(marks[i]) == "Passed")
+            res.add(studentList.get(marks[i]));
     }
-    return ans+"\"";
+    return String.join(",", res);
     }
   }
